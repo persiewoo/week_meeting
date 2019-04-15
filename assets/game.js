ee6df99bbc3644b988c6c3c03f4401ec7d6941f0
wood <xianbei2@126.com>
2019-04-15 09:58:36 +0800

    【DEV】V2.0.0

--- assets/game.js
+++ assets/game.js
@@ -6,18 +6,26 @@ cc.Class({
         lbl_btn : cc.Label,
         btn : cc.Button,
         duration : 0.05,
+        node_item : cc.Node,
+        node_notice : cc.Node,
+        fr_heads: cc.SpriteAtlas,
+        img_head : cc.Sprite,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        this.members =
         this.name_arr = [];
         this.play_ind = 0;
-        cc.loader.loadRes("team.json",null,(err,arr) => {
+        this.total_weight = 0;
+        this.play_times = 99999999;
+        cc.loader.loadRes("team.json",null,(err,_obj) => {
             if(!err){
-                this.name_arr = arr;
+                this.update_members(_obj);
                 this.play_ind = Math.floor(Math.random()*1000) % this.name_arr.length;
-                this.update_ui();
+                this.lbl_name.string = this.members[this.name_arr[this.play_ind]].name;
+                this.img_head.spriteFrame = this.fr_heads.getSpriteFrame(this.name_arr[this.play_ind]);
             }
         });
         this.schedule_cb = function () {
@@ -48,7 +56,39 @@ cc.Class({
     },
 
     update_ui:function () {
-        this.lbl_name.string = this.name_arr[this.play_ind];
+        let show_name = this.members[this.name_arr[this.play_ind]].name
+        this.lbl_name.string = show_name;
+        this.img_head.spriteFrame = this.fr_heads.getSpriteFrame(this.name_arr[this.play_ind]);
+        --this.play_times;
+        if(this.play_times === 20){
+            this.unschedule(this.schedule_cb);
+            this.schedule(this.schedule_cb,this.duration*2.5);
+        }
+        else if(this.play_times === 10){
+            this.unschedule(this.schedule_cb);
+            this.schedule(this.schedule_cb,this.duration*3);
+        }
+        else if(this.play_times === 5){
+            this.unschedule(this.schedule_cb);
+            this.schedule(this.schedule_cb,this.duration*4);
+        }
+        else if(this.play_times === 3){
+            this.unschedule(this.schedule_cb);
+            this.schedule(this.schedule_cb,this.duration*5);
+        }
+        if(this.choose_user && this.play_times <= 0 && show_name === this.choose_user.name){
+            this.unschedule(this.schedule_cb);
+            this.members[this.choose_user.id] = null;
+            this.update_members(this.members);
+            this.lbl_btn.string = "Start";
+            this.btn.interactable = true;
+            let item = cc.instantiate(this.node_item);
+            item.x = 0;
+            item.getChildByName("name").getComponent(cc.Label).string = this.choose_user.name;
+            cc.find("node_icon/img_icon",item).getComponent(cc.Sprite).spriteFrame = this.fr_heads.getSpriteFrame(this.choose_user.id);
+            item.parent = this.node_notice;
+            this.choose_user = null;
+        }
     },
 
     on_start:function () {
@@ -65,9 +105,12 @@ cc.Class({
 
     on_end:function () {
         cc.log("on_end");
-        this.lbl_btn.string = "Start";
+        this.btn.interactable = false;
+        let rand_ind = Math.floor(Math.random()*this.total_weight);
+        this.check_user(rand_ind);
+        this.play_times = 30;
         this.unschedule(this.schedule_cb);
-        this.name_arr.splice(this.play_ind,1);
+        this.schedule(this.schedule_cb,this.duration*2);
     },
 
     on_click_btn:function () {
@@ -77,6 +120,30 @@ cc.Class({
         }
         else{
             this.on_end();
+        }
+    },
+
+    check_user:function(_ind){
+        for(let k in this.members){
+            if(this.members[k] && _ind >= this.members[k].w_start && _ind <= this.members[k].w_end ){
+                this.choose_user = this.members[k];
+                return;
+            }
+        }
+    },
+
+    update_members:function (_obj) {
+        this.members = _obj;
+        this.name_arr = [];
+        this.total_weight = 0;
+        for(let _key in _obj){
+            if(!_obj[_key]){
+                continue;
+            }
+            _obj[_key].w_start = this.total_weight;
+            this.total_weight += _obj[_key].weight + _obj[_key].ext_w;
+            _obj[_key].w_end = this.total_weight - 1;
+            this.name_arr.push(_obj[_key].id);
         }
     },
 
